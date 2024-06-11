e4f53f0a356ecb797a529243cb0298f72d708bc0
Chandler J <cjustice2000@gmail.com>
2024-06-11 12:05:26 -0600

    fixed interaction end behavior

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    id: string;
-    start: (chart: any, _args: any, options: any) => void;
-    beforeUpdate: (chart: any, args: any, options: any) => void;
-    afterDraw: (chart: any, args: any, options: any) => void;
-    setSelection: (chart: any, range?: any[]) => void;
-    clearSelection: (chart: any) => void;
-};
-export default _default;
